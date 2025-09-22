e316acafffc4456c98bc4c461b4e7daef5effb83
rladbwls77 <K51165480@gmail.com>
2025-09-22 23:57:08 +0900

    배열

--- arrangement/arrangement/main.c
+++ arrangement/arrangement/main.c
@@ -9,26 +9,23 @@ int main()
 	char b[NUM];
 	int i;
 
-	// 랜덤 시드 설정 (매번 다른 난수)
-	srand((unsigned)time(NULL));
-
 	for (i = 0; i < NUM; i++) {
 		a[i] = i;
 		b[i] = i;
 	}
 
-	for (i = 0; i < NUM; i++) {
-		int j = rand() % NUM;   // 0~9 중 랜덤 위치 선택
-		// swap a[i] <-> a[j]
+	for (i = 0; i < NUM; i++) 
+	{
+		int j = rand() % NUM;   
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
 
 
-	for (i = 0; i < NUM; i++) {
-		int j = rand() % NUM;   // 0~9 중 랜덤 위치 선택
-		// swap a[i] <-> a[j]
+	for (i = 0; i < NUM; i++) 
+	{
+		int j = rand() % NUM;   
 		int temp = b[i];
 		b[i] = b[j];
 		b[j] = temp;
@@ -36,12 +33,14 @@ int main()
 
 
 	printf("a[] :");
-	for (int i = 0; i < NUM; i++) {
+	for (int i = 0; i < NUM; i++) 
+	{
 		printf(" % d", a[i]);
 	}
 
 	printf("\nb[] :");
-	for (int i = 0; i < NUM; i++) {
+	for (int i = 0; i < NUM; i++) 
+	{
 		printf(" % d", b[i]);
 	}
 	return 0;
