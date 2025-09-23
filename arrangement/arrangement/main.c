3eb0925b5157dadd09a4637ad3d7e82f3f9fc461
rladbwls77 <K51165480@gmail.com>
2025-09-23 17:12:47 +0900

    설명

--- arrangement/arrangement/main.c
+++ arrangement/arrangement/main.c
@@ -11,7 +11,7 @@ int main()
 
 	for (i = 0; i < NUM; i++)//10번 반복 
 	{
-		a[i] = i; // 10이 들어감 
+		a[i] = i; // 숫자가 0~9가 들어감 순서대로 들어 갑니다.
 		b[i] = i;
 	}
 
@@ -19,21 +19,21 @@ int main()
 	{
 		int j = rand() % NUM;   
 		int temp = a[i];
-		a[i] = a[j];
+		a[i] = a[j];//이렇게 옮기는 과정이 없으면 중복이 된다.(0~9까지 나오는 i의 값을 랜덤한 숫자를 넣은 J의 값을 넣는다)
 		a[j] = temp;
 	}
 
 	for (i = 0; i < NUM; i++) 
 	{
-		int j = rand() % NUM;   
+		int j = rand() % NUM; 
 		int temp = b[i];
-		b[i] = b[j]; //이렇게 옮기는 과정이 없으면 중복이 된다.
+		b[i] = b[j]; //이렇게 옮기는 과정이 없으면 중복이 된다.(0~9까지 나오는 i의 값을 랜덤한 숫자를 넣은 J의 값을 넣는다)
 		b[j] = temp;
 	}
 
 
 	printf("a[] :"); //앞에 표시 
-	for (int i = 0; i < NUM; i++) 
+	for (int i = 0; i < NUM; i++) //아래 내용을 10번 반복 
 	{
 		printf(" % d", a[i]);
 	}
